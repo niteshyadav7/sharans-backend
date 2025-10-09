127be0e68c21c70cf88c4bf6d8efdbc2fb542739
niteshyadav7 <nitesh0709yadav@gmail.com>
2025-10-09 13:58:24 +0530

    my backend working perfectly

--- server.js
+++ server.js
@@ -70,7 +70,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://sharans-backend.onrender.com", credentials: true }));
 app.use(helmet());
 app.use(compression());
 
